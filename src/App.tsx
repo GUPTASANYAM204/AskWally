45d22173a3f458226f8f2dcc99f3ea16dbb02873
Arushi <arushijain866@gmail.com>
2025-06-30 09:04:07 +0530

    Add Terms of Service page

--- src/App.tsx
+++ src/App.tsx
@@ -15,50 +15,52 @@ import { StoreMapPage } from './pages/StoreMapPage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrderConfirmationPage } from './pages/OrderConfirmationPage';
 import ProfilePage from './pages/ProfilePage';
+import { TermsPage } from './pages/TermsPage';
 
 function App() {
   return (
     <AuthProvider>
       <CartProvider>
         <OrderProvider>
-          <Router>
-            <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-yellow-50 font-inter">
-              <Navigation />
-              <CartDrawer />
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/products" element={<ProductsPage />} />
-                <Route path="/product/:id" element={<ProductDetailPage />} />
-                <Route path="/store-map" element={<StoreMapPage />} />
-                <Route 
-                  path="/checkout" 
-                  element={
-                    <ProtectedRoute>
-                      <CheckoutPage />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/order-confirmation" 
-                  element={
-                    <ProtectedRoute>
-                      <OrderConfirmationPage />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/profile" 
-                  element={
-                    <ProtectedRoute>
-                      <ProfilePage />
-                    </ProtectedRoute>
-                  } 
-                />
-              </Routes>
-            </div>
-          </Router>
+            <Router>
+              <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-yellow-50 font-inter">
+                <Navigation />
+                <CartDrawer />
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/signup" element={<SignupPage />} />
+                  <Route path="/products" element={<ProductsPage />} />
+                  <Route path="/product/:id" element={<ProductDetailPage />} />
+                  <Route path="/store-map" element={<StoreMapPage />} />
+                  <Route 
+                    path="/checkout" 
+                    element={
+                      <ProtectedRoute>
+                        <CheckoutPage />
+                      </ProtectedRoute>
+                    } 
+                  />
+                  <Route 
+                    path="/order-confirmation" 
+                    element={
+                      <ProtectedRoute>
+                        <OrderConfirmationPage />
+                      </ProtectedRoute>
+                    } 
+                  />
+                  <Route 
+                    path="/profile" 
+                    element={
+                      <ProtectedRoute>
+                        <ProfilePage />
+                      </ProtectedRoute>
+                    } 
+                  />
+                  <Route path="/terms" element={<TermsPage />} />
+                </Routes>
+              </div>
+            </Router>
         </OrderProvider>
       </CartProvider>
     </AuthProvider>
